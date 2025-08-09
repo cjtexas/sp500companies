27ed099d4593881a0d8fa66afc7d0bfee454b2e9
Caleb Jenkins <onehundredpercenttexan@gmail.com>
2025-08-09 13:23:25 -0500

    use write_csv()

--- R/harvest_sp500.r
+++ R/harvest_sp500.r
@@ -1,7 +1,8 @@
+library(readr)
 library(rvest)
 
 tbl <- html_table(read_html("https://en.wikipedia.org/wiki/List_of_S%26P_500_companies#S&P_500_component_stocks"))[[1]]
 
 if (!dir.exists("./data")) dir.create("./data", recursive = TRUE)
 
-write.csv(tbl, file = "./data/sp500.csv", row.names = FALSE)
+write_csv(tbl, file = "./data/sp500.csv")
